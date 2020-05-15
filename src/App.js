cb5d32768707cb17545a5415f2aa9b6b562110a2
Aleksey Baranov <63651656+njhnehf@users.noreply.github.com>
2020-05-15 09:48:36 +0300

    oopps

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://dry-savannah-52432.herokuapp.com/:3000/image', {
+          fetch('https://dry-savannah-52432.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
